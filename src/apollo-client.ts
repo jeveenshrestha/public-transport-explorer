3252ae277082fe73e30a4aff3c7a0ec796571cab
Jeveen Shrestha <jeveen2012shrestha@gmail.com>
2025-03-05 21:46:18 +0200

    change service area from only HSL to Finland

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.digitransit.fi/routing/v2/hsl/gtfs/v1',
+    uri: 'https://api.digitransit.fi/routing/v2/finland/gtfs/v1',
     headers: {
       'digitransit-subscription-key': import.meta.env.VITE_HSL_API_KEY,
     },
